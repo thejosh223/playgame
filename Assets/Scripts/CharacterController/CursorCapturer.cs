d6364afa88d5352914384e424df257e07331668a
unknown <Josh@Josh-PC.(none)>
2014-03-29 22:26:17 +0800

    Initial Commit

---
+++ Assets/Scripts/CharacterController/CursorCapturer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CursorCapturer : MonoBehaviour {	
+	private MouseLook[] mouseLooks;
+	private bool _captureCursor = true;
+
+	void Awake() {
+		_singleton = this;
+	}
+	
+	void Start() {
+		mouseLooks = GetComponentsInChildren<MouseLook>();
+	}
+	
+	void Update() {
+		/*
+		 * Capturing of the Camera
+		 */
+		if (_captureCursor) {
+			Screen.lockCursor = true;
+			for (int i = 0; i < mouseLooks.Length; i++)
+				mouseLooks[i].enabled = Screen.lockCursor;
+		} else {
+			Screen.lockCursor = false;
+			for (int i = 0; i < mouseLooks.Length; i++)
+				mouseLooks[i].enabled = false;
+		}
+	}
+	
+	public bool CaptureCursor {
+		get { return _captureCursor;}
+		set { _captureCursor = value; }
+	}
+	
+	private static CursorCapturer _singleton;
+	
+	public static CursorCapturer Instance {
+		get { return _singleton; }
+	}
+}
---
+++ Assets/Scripts/ObjectPools/ObjectPool.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T> : MonoBehaviour where T : Component {
+	// 1st dimension = object types, 2nd = objects in the pool for that type
+	readonly List<T> objects;
+	// Can set a parent folder to store objects in the pool in
+	public Transform ParentFolder;
+	// Static pointer to an instance of this
+	public static ObjectPool<T> Instance;
+	// Activating and deactivating objects can be slow - almost as slow as instantiating them
+	// Set to false to leave objects active
+	public bool AutoActive = true;
+	// Number to allocate at once when more are needed
+	public int NumToAllocate = 5;
+	
+	public ObjectPool () {
+		objects = new List<T>();
+		Instance = this;
+	}
+	
+	// Create new objects and but them in the pool
+	public void Allocate(T obj, int number) {
+		// TODO: Can we run the object's Awake/Start methods, and then clone THAT one that's already set up, further reducing creation time?
+		for (int i = 0; i < number; i++) {
+			T newObj = (T)Instantiate(obj);
+			newObj.name = obj.name; // Get rid of the "(clone)" designation
+			Add(newObj);
+		}
+	}
+	
+	// Get an object from the pool
+	public T Get(T obj) {
+		if (objects.Count == 0) {
+			Allocate(obj, NumToAllocate);
+		}
+		T returnObj = objects[0];
+		objects.RemoveAt(0);
+
+		if (AutoActive)
+			returnObj.gameObject.SetActive(true);
+		return returnObj;
+	}
+	
+	// Add an object into the pool
+	public void Add(T obj) {
+		if (AutoActive)
+			obj.gameObject.SetActive(false);
+		obj.transform.parent = ParentFolder; // Keep things tidy
+		objects.Add(obj);
+	}
+}
